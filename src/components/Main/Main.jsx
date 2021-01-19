4c1151e1155f90c8244eeb348854685c018ae6dc
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-19 06:45:56 +0530

    main and form components structure is done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
     const classes= useStyles();
@@ -14,7 +15,7 @@ const Main = () => {
                     Say Something
                  </Typography>
                  <Divider />
-                 {/* <Form></Form> */}
+                 <Form />
             </CardContent>
             <CardContent className={classes.CardContent} >
                 <Grid container spacing={2} >

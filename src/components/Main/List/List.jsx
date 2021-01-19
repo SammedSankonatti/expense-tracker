25e044d88936b47d8b3e2dde772c5bd057da0dcd
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-20 01:41:20 +0530

    The delete transaction is impleted

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -23,8 +23,8 @@ const List = () => {
                             </ListItemAvatar>
                             <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
                             <ListItemSecondaryAction >
-                                <IconButton edge="end" aria-label="delete" onClick="" >
-                                    <Delete />
+                                <IconButton edge="end" aria-label="delete" onClick={()=> deleteTransaction(transaction.id)} >
+                                    <Delete  />
                                 </IconButton>
                             </ListItemSecondaryAction>
                         </ListItem>

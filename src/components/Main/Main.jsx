ac6e8d863e1708807454d156e3b6cbbac0c3696d
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-20 10:44:47 +0530

    few modifications

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,11 +9,14 @@ const Main = () => {
     const classes= useStyles();
     return (
         <Card className={classes.root} >
-            <CardHeader title="Expense tracker" subheader="speechly powered app" />
+            <CardHeader style={{color:'rgb(0,128,128)', paddingLeft:'4rem'}}  title="TRACK YOUR EXPENSES" />
             <CardContent>
-                <Typography  align="center" variant="h5"  >Total balance $2000</Typography>
-                <Typography variant="subtitle1" style={{lineHeight:'1.5em', marginTop:'20px'}} > 
-                    Say Something
+                <Typography  align="center" variant="h5" style={{color:'rgba(103, 65, 114, 1)'}}  >Total balance $2000</Typography>
+                <Typography align="center" variant="h6" style={{lineHeight:'1.5em', marginTop:'20px', color:'rgba(52, 73, 94, 1)'}} > 
+                    Enter Transactions: 
+                 </Typography>
+                 <Typography style={{color:'rgba(30, 130, 76, 1', fontSize:'15px', fontWeight:'600', paddingBottom:'1rem'}} >
+                     Hint : Try saying add transaction for 100 dollars in category sports next friday
                  </Typography>
                  <Divider />
                  <Form />
